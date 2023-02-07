7e9f3002d18e8554190b3ce10afe8ab94d42a53d
HellHorseman <duke.sheffer@yandex.ru>
2023-02-07 16:41:54 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+    }
+
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+
+    }
+
+    private static void task3() {
+    }
+
+    private static void task4() {
+    }
+
+    private static void task5() {
+    }
+
+    private static void task6() {
+    }
+
+    private static void task7() {
+    }
+
+    private static void task8() {
+    }
+}
