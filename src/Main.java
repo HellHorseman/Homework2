e177f6e413afa93cee9266db79769df6c4592d64
HellHorseman <duke.sheffer@yandex.ru>
2023-02-07 17:02:42 +0400

    add task 5

--- src/Main.java
+++ src/Main.java
@@ -59,6 +59,14 @@ public class Main {
 
     private static void task5() {
         System.out.println("Задача 5");
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
     }
 
     private static void task6() {
