e3e6dfd9536261769f9b7fd210c8ef4f676ef090
HellHorseman <duke.sheffer@yandex.ru>
2023-02-07 16:51:12 +0400

    add task 3

--- src/Main.java
+++ src/Main.java
@@ -35,6 +35,16 @@ public class Main {
     }
 
     private static void task3() {
+        System.out.println("Задача 3");
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
     }
 
     private static void task4() {
